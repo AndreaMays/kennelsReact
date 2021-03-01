c6db99efff171b139fe998089326d033970b378a
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-03-01 11:53:07 -0600

    Finished Ch1: Added Cards & CSS for Location, Employee, Customer, Animal Card

---
+++ src/components/empoloyee/Employee.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Employee.css"
+
+export const Employee = () => (
+ 
+    <employee class="employee" >
+        <div>Emma Beaton</div>
+        <div>Nashville Kennels North</div>
+    </employee>
+
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Location.css"
+
+export const Location = () => (
+ 
+    <address class="location">
+        <div>Visit Us at the Nashville North Location</div>
+        <div>500 Puppy Way</div>
+    </address>
+
+)
