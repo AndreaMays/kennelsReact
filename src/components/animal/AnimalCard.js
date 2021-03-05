d1feaf8ea1ee3ce30d82d81877a077d1da72251b
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-03-05 14:34:12 -0600

    Added Ch9 and Ch 10: AnimalForm not rendering, successfully added user auth

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,13 +1,13 @@
 import React from "react"
 import "./Animal.css"
 
-export const AnimalCard = ({ animal, customer, location }) => (
+export const AnimalCard = ({ animal, customer}) => (
 
     <section className="animal">
         <h3 className="animal__name">Name: {animal.name}</h3>
         <address className="animal__location">Location: {animal.location.name}</address>
-        <breed class="animal__breed">Breed: {animal.breed} </breed>
-        <div className="animal__owner"> Owner: {customer.name}</div>
+        <div className="animal__breed">Breed: {animal.breed} </div>
+        <div>Owner: {customer.name}</div>
     </section>
 
 )
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -3,13 +3,17 @@ import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
 import { LocationContext } from "../location/LocationProvider"
 import { CustomerContext } from "../customer/CustomerProvider"
+import { useHistory } from "react-router-dom"
 import "./Animal.css"
+
 
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { animals, getAnimals } = useContext(AnimalContext)
   const { locations, getLocations } = useContext(LocationContext)
   const { customers, getCustomers } = useContext(CustomerContext)
+  
+  const history = useHistory()
 
   //useEffect - reach out to the world for something
   useEffect(() => {
@@ -23,6 +27,10 @@ export const AnimalList = () => {
   return (
       <>
     <div className="animals">
+      {/* <h2>Animals</h2> */}
+            <button onClick={() => {history.push("/animals/create")}}>
+              "Add Animal"
+            </button>
       {/* {console.log("AnimalList: Render", animals)} */}
       {
         animals.map(animal => {
